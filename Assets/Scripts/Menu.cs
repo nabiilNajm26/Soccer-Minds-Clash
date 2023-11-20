69e4f65bc408719c8bd907e65ff195fd0e37756a
Nabil Najmudin <nabiil.najm266@gmail.com>
2023-11-20 20:05:43 +0700

    Updating scripts with comments
    Adding some comments to make it easier to understand the code

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,24 +1,32 @@
+// Import library atau namespace yang diperlukan
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Deklarasi kelas Menu yang merupakan turunan dari MonoBehaviour
 public class Menu : MonoBehaviour
 {
+    // Deklarasi variabel publik yang dapat diakses dari Editor Unity
     public GameObject panelLoading, panelTransit;
     public Image img_loading;
     public static bool isLoading = false;
-    public Text txt_loading; 
+    public Text txt_loading;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(isLoading == false)
+        // Memulai fungsi Start saat aplikasi/game pertama kali dijalankan
+
+        // Memeriksa apakah isLoading bernilai false
+        if (isLoading == false)
         {
+            // Jika isLoading false, memulai coroutine waitLoadingMenu
             StartCoroutine(waitLoadingMenu());
         }
         else
         {
+            // Jika isLoading true, menonaktifkan panelLoading
             panelLoading.SetActive(false);
         }
     }
@@ -26,29 +34,48 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(img_loading.fillAmount < 1)
+        // Memulai fungsi Update setiap frame
+
+        // Memeriksa apakah fillAmount dari img_loading kurang dari 1
+        if (img_loading.fillAmount < 1)
         {
+            // Jika ya, menambahkan fillAmount img_loading sebesar 0.005 setiap frame
             img_loading.fillAmount += 0.005f;
         }
-        if(img_loading.fillAmount >= 1)
+
+        // Memeriksa apakah fillAmount img_loading sudah mencapai atau melebihi 1
+        if (img_loading.fillAmount >= 1)
         {
+            // Jika ya, mengubah nilai isLoading menjadi true
             isLoading = true;
         }
-        txt_loading.text = Mathf.RoundToInt(img_loading.fillAmount * 100f) + "%";
 
+        // Menampilkan persentase loading pada txt_loading
+        txt_loading.text = Mathf.RoundToInt(img_loading.fillAmount * 100f) + "%";
     }
+
+    // Coroutine untuk menunggu loading menu
     IEnumerator waitLoadingMenu()
     {
-        yield return new WaitForSeconds(3f);
+        // Menunggu selama 3 detik
+        yield return new WaitForSeconds(5f);
+
+        // Menampilkan panelTransit dan menunggu selama 1.5 detik
         panelTransit.SetActive(true);
         yield return new WaitForSeconds(1.5f);
+
+        // Menonaktifkan panelLoading dan menunggu selama 1.5 detik
         panelLoading.SetActive(false);
         yield return new WaitForSeconds(1.5f);
+
+        // Menonaktifkan panelTransit setelah proses loading selesai
         panelTransit.SetActive(false);
     }
 
+    // Method yang dipanggil saat tombol play ditekan
     public void ButtonPlay()
     {
+        // Memuat level "Play"
         Application.LoadLevel("Play");
     }
 }
--- Assets/Scripts/TeamUI.cs
+++ Assets/Scripts/TeamUI.cs
@@ -1,34 +1,53 @@
+// Import library atau namespace yang diperlukan
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Deklarasi kelas TeamUI yang merupakan turunan dari MonoBehaviour
 public class TeamUI : MonoBehaviour
 {
+    // Deklarasi variabel instance yang bersifat statis, dapat diakses dari kelas lain
     public static TeamUI instance;
+
+    // Array untuk menyimpan sprite dari bendera tim
     public Sprite[] TeamFlag;
+
+    // Array untuk menyimpan nama tim
     public string[] TeamName;
+
+    // Array untuk menyimpan sprite dari bintang
     public Sprite[] Star;
+
+    // Method yang dipanggil saat objek dibuat di dunia game
     private void Awake()
     {
-        if(instance == null)
+        // Memastikan bahwa hanya ada satu instance dari TeamUI yang dapat ada
+        if (instance == null)
         {
+            // Jika instance belum ada, set instance menjadi objek saat ini
             instance = this;
+
+            // Menyatakan bahwa objek ini tidak akan dihancurkan ketika pindah antar level
             DontDestroyOnLoad(gameObject);
         }
         else
         {
+            // Jika instance sudah ada, hancurkan objek saat ini agar hanya ada satu instance yang tersisa
             DestroyImmediate(gameObject);
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Fungsi Start yang dipanggil sebelum frame pertama dijalankan
+        // (Tidak ada implementasi khusus di sini)
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Fungsi Update yang dipanggil setiap frame
+        // (Tidak ada implementasi khusus di sini)
     }
 }
